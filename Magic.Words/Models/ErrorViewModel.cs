1c78999949a990315a9596111fef04606d653ee2
Serhii Holyboroda <49484600+SerhiiHolyboroda@users.noreply.github.com>
2023-12-08 17:23:38 +0200

    Adding main files

---
+++ Magic.Words/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace Magic.Words.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
